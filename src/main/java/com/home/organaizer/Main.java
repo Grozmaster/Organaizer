1a692926c0f4f9915be93063b7d2d59505d8f0be
grozmaster <grozmaster@ukr.net>
2019-08-08 15:46:25 +0300

    update git ignore

--- src/main/java/com/home/organaizer/Main.java
+++ src/main/java/com/home/organaizer/Main.java
@@ -3,7 +3,6 @@ package com.home.organaizer;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!I Organaizer!)");
-        System.out.println("Hello");
     }
 
 }
