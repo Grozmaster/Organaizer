01cbfe5f70babd7e1d77cb3dcf980318e3429b46
grozmaster <grozmaster@ukr.net>
2019-08-08 14:57:41 +0300

    first commit

---
+++ src/main/java/com/home/organaizer/Main.java
@@ -0,0 +1,9 @@
+package com.home.organaizer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi!I Organaizer!)");
+        System.out.println("Hello!");
+    }
+
+}
